acda04f4862be3e5d74be2220c1e09e2d09b279e
Chris Baltazar <ing.chris@hotmail.com>
2025-03-21 00:49:46 +0100

    chore(Sales): form submit

--- resources/js/Pages/Sales/Index.vue
+++ resources/js/Pages/Sales/Index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {Head} from '@inertiajs/vue3';
+import {Head, useForm} from '@inertiajs/vue3';
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {ref, computed} from "vue";
 
@@ -13,12 +13,14 @@ const columns = [
     {data: 'price'},
 ];
 
-const info = ref({
-    salesman: '',
+const salesman = ref('');
+
+const data = ref({
     product: '',
     size: '',
     price: ''
 });
+
 const details = ref([]);
 
 const hasDetails = computed(() => {
@@ -26,19 +28,35 @@ const hasDetails = computed(() => {
 });
 
 const addDetail = () => {
-    if (info.value.product && info.value.size && info.value.price) {
-        const detailRow = {...info.value}
-        delete detailRow.salesman;
-        details.value.push(detailRow);
+    if (data.value.product && data.value.size && data.value.price) {
+        details.value.push({...data.value});
         clear();
     }
 }
 
 const clear = (reset = false) => {
-    info.value = {}
+    data.value = {}
     if (reset) {
+        salesman.value = '';
         details.value = [];
     }
+}
+
+const submit = (type) => {
+    const form = useForm({
+        type: type,
+        salesman: salesman.value,
+        details: details.value
+    });
+
+    form.post(route('sales.store'), {
+        onSuccess: () => {
+            clear(true);
+        },
+        onError: () => {
+            console.log('Error');
+        }
+    });
 }
 
 </script>
@@ -53,19 +71,21 @@ const clear = (reset = false) => {
             <div class="row">
                 <div class="col">
                     <div class="form-floating">
-                        <input type="text" class="form-control" v-model="info.salesman" id="txtSalesman" placeholder="Vendedor">
+                        <input type="text" class="form-control" v-model="salesman" id="txtSalesman"
+                               placeholder="Vendedor">
                         <label for="txtSalesman">Vendedor</label>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-floating">
-                        <input type="text" class="form-control" v-model="info.product" id="txtProduct" placeholder="Producto">
+                        <input type="text" class="form-control" v-model="data.product" id="txtProduct"
+                               placeholder="Producto">
                         <label for="txtProduct">Producto</label>
                     </div>
                 </div>
                 <div class="col">
                     <div class="form-floating">
-                        <select class="form-control" v-model="info.size" id="cmbSize">
+                        <select class="form-control" v-model="data.size" id="cmbSize">
                             <option v-for="size in sizes" :value="size" :key="size">{{ size }}</option>
                         </select>
                         <label for="cmbSize">Talla</label>
@@ -73,7 +93,8 @@ const clear = (reset = false) => {
                 </div>
                 <div class="col">
                     <div class="form-floating">
-                        <input type="number" step="1" class="form-control" v-model="info.price" id="txtPrice" placeholder="Precio">
+                        <input type="number" step="1" class="form-control" v-model="data.price" id="txtPrice"
+                               placeholder="Precio">
                         <label for="txtPrice">Precio</label>
                     </div>
                 </div>
@@ -117,7 +138,9 @@ const clear = (reset = false) => {
             <div class="d-flex justify-content-center">
                 <button type="button" class="btn btn-danger my-1 mx-1" @click="clear(true)">Cancelar</button>
                 <button type="button" :disabled="!hasDetails" class="btn btn-info my-1 mx-1">Tarjeta</button>
-                <button type="button" :disabled="!hasDetails" class="btn btn-success my-1 mx-1">Venta Simple</button>
+                <button type="button" :disabled="!hasDetails" class="btn btn-success my-1 mx-1"
+                        @click="submit('simple')">Venta Simple
+                </button>
                 <button type="button" :disabled="!hasDetails" class="btn btn-warning my-1 mx-1">Venta Fiscal</button>
             </div>
         </div>
