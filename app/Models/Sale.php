9db2db0a8dd66e07660473851195e318ca0cc88c
Chris Baltazar <ing.chris@hotmail.com>
2025-03-22 12:32:01 +0100

    chore(models): sales & related

---
+++ app/Models/Sale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Sale extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function details()
+    {
+        return $this->hasMany(SaleDetail::class, 'sale_id');
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class, 'store_id');
+    }
+
+    public function salesman()
+    {
+        return $this->belongsTo(Staff::class, 'staff_id');
+    }
+}
