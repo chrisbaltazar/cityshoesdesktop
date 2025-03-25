a9a3103698403b16a6e9f0091b4e864ef49105f9
Chris Baltazar <ing.chris@hotmail.com>
2025-03-26 00:12:25 +0100

    chore(Store): factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             StaffSeeder::class,
+            StoreSeeder::class,
         ]);
     }
 }
