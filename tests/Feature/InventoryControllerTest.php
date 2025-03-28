89b3851f72e90ed05b6c4149d4b4ef6a746fa164
Chris Baltazar <ing.chris@hotmail.com>
2025-03-28 17:24:52 +0100

    test(InventoryController): rename and extend

--- tests/Feature/InventoryControllerTest.php
+++ tests/Feature/InventoryControllerTest.php
@@ -2,25 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ProductControllerTest extends TestCase
+class InventoryControllerTest extends TestCase
 {
 
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     */
     public function test_index(): void
     {
         $user = User::factory()->create();
+        Product::factory()->count(10)->create();
+
         $response = $this
             ->actingAs($user)
             ->get('/inventory');
 
         $response->assertOk();
+        $response->assertInertia(fn ($page) => $page->has('products', 10));
     }
 }
